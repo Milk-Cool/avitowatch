eac42636230e4bc383451a2b1a2911837f27cb8e
Milk-Cool <milan-pl@mail.ru>
2024-03-14 13:46:00 +0300

    v2

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const jsdom = require("jsdom");
+const puppeteer = require("puppeteer");
 const TelegramBot = require("node-telegram-bot-api");
 const fs = require("fs");
-const { HttpsProxyAgent } = require("https-proxy-agent");
-
-const { JSDOM } = jsdom;
 
 const FILE = "listings.txt";
 
@@ -20,29 +17,32 @@ if(!fs.existsSync(FILE))
 let listings = fs.readFileSync(FILE, "utf-8").split("\n").filter(x => x);
 const saveListings = () => fs.writeFileSync(FILE, listings.join("\n"));
 
-const bot = new TelegramBot(TOKEN, { "polling": true });
+const bot = new TelegramBot(TOKEN, { "polling": false });
 
+let browser = puppeteer.launch({ "headless": false });
 const check = async () => {
-    const f = await fetch(URL, {
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-        }
-    });
-    const t = await f.text();
+    if(browser instanceof Promise)
+        browser = await browser;
+    const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+    await page.goto(URL);
 
-    console.log(t);
+    await page.waitForSelector("[elementtiming='bx.catalog.container']");
 
-    const dom = new JSDOM(t, { "contentType": "text/html" });
-    const items = dom.window.document.querySelectorAll("[data-marker='item']");
+    const items = await page.$$("[data-marker='item']");
     for(let i of items) {
-        const id = i.getAttribute("data-item-id");
-        if(listings.includes(id)) continue;
-        listings.push(id);
-        saveListings();
-        const name = i.querySelector("meta[itemprop='description']").textContent;
-        const url = i.querySelector("a[itemprop][data-marker]").getAttribute("href");
-        bot.sendMessage(ID, name + "\n\n" + url);
+        try {
+            const id = await page.evaluate(el => el.getAttribute("data-item-id"), i);
+            console.log(id);
+            if(listings.includes(id)) continue;
+            listings.push(id);
+            saveListings();
+            const name = await page.evaluate(el => el.querySelector("h3[itemprop='name']").innerText, i);
+            const url = await page.evaluate(el => el.querySelector("a[itemprop][data-marker]").getAttribute("href"), i);
+            await bot.sendMessage(ID, name + "\n\n" + "https://avito.ru" + url);
+        } catch(e) { console.error(e); }
     }
+    await page.close();
 };
 
 setTimeout(check); // Check immediately
